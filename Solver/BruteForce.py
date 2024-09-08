174240edae1607159ee632486b773ab06b9c08c9
Georg Jung <georgjung@acm.org>
2024-09-08 10:36:28 +0200

    Added puzzle for September 2024.

--- Solver/BruteForce.py
+++ Solver/BruteForce.py
@@ -1,7 +1,7 @@
 '''
 Created on 15.02.2021
 
-__updated__='2024-08-12'
+__updated__='2024-09-08'
 
 @author: jung
 '''
@@ -472,6 +472,16 @@ Aug2024 = np.array([[0, 0, 0, 0, 0, 1, 0, 0, 0],
                     [0, 0, 1, 4, 2, 0, 6, 0, 0],
                     [0, 0, 0, 3, 0, 0, 0, 0, 0]])
 
+Sep2024 = np.array([[0, 4, 0, 0, 0, 5, 0, 0, 0],
+                    [0, 7, 0, 0, 0, 0, 0, 4, 1],
+                    [0, 0, 0, 8, 0, 6, 0, 0, 0],
+                    [6, 0, 9, 0, 0, 0, 2, 0, 0],
+                    [0, 0, 0, 0, 9, 0, 0, 0, 0],
+                    [0, 0, 3, 0, 0, 0, 5, 0, 6],
+                    [0, 0, 0, 3, 0, 4, 0, 0, 0],
+                    [5, 2, 0, 0, 0, 0, 0, 7, 0],
+                    [0, 0, 0, 1, 0, 0, 0, 5, 0]])
+
 # All puzzles
 Pzzls = {"Empty puzzle": Pzzl_xx,
          "Sudoku book, cover": Pzzl_00,
@@ -518,7 +528,8 @@ Pzzls = {"Empty puzzle": Pzzl_xx,
          "Alverde magazine, May 2024": May2024,
          "Alverde magazine, June 2024": Jun2024,
          "Alverde magazine, July 2024": Jul2024,
-         "Alverde magazine, August 2024": Aug2024}
+         "Alverde magazine, August 2024": Aug2024,
+         "Alverde magazine, September 2024": Sep2024}
 
 
 def solve(Pzzl, a):
