dace71424f70aa8e2ac32c75ae185bb37609d797
Georg Jung <georgjung@acm.org>
2025-04-06 17:49:04 +0200

    Added puzzle for April 2025.

--- Solver/BruteForce.py
+++ Solver/BruteForce.py
@@ -1,7 +1,7 @@
 '''
 Created on 15.02.2021
 
-__updated__='2025-03-04'
+__updated__='2025-04-06'
 
 @author: jung
 '''
@@ -542,6 +542,16 @@ Mar2025 = np.array([[0, 5, 0, 0, 1, 0, 0, 9, 0],
                     [3, 0, 4, 0, 0, 6, 0, 0, 1],
                     [0, 7, 0, 0, 2, 0, 0, 5, 0]])
 
+Apr2025 = np.array([[5, 9, 0, 0, 0, 0, 0, 0, 7],
+                    [6, 4, 0, 0, 1, 0, 0, 2, 0],
+                    [0, 0, 2, 0, 0, 8, 4, 0, 0],
+                    [0, 0, 0, 9, 0, 0, 6, 0, 0],
+                    [0, 1, 0, 0, 3, 0, 0, 8, 0],
+                    [0, 0, 3, 0, 0, 5, 0, 0, 0],
+                    [0, 0, 5, 4, 0, 0, 3, 0, 0],
+                    [0, 2, 0, 0, 8, 0, 0, 1, 6],
+                    [9, 0, 0, 0, 0, 0, 0, 7, 4]])
+
 # All puzzles
 Pzzls = {"Empty puzzle": Pzzl_xx,
          "Sudoku book, cover": Pzzl_00,
@@ -595,7 +605,8 @@ Pzzls = {"Empty puzzle": Pzzl_xx,
          "Alverde magazine, December 2024": Dec2024,
          "Alverde magazine, January 2025": Jan2025,
          "Alverde magazine, February 2025": Feb2025,
-         "Alverde magazine, March 2025": Mar2025}
+         "Alverde magazine, March 2025": Mar2025,
+         "Alverde magazine, April 2025": Apr2025}
 
 
 def solve(Pzzl, a):
